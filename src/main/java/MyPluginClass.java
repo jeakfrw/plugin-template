7f0a8ef17ba29af8a5d7c4806d07f226275a7f72
Magnus Leßmann <github@m-lessmann.de>
2021-02-25 22:32:09 +0100

    🐘 Update gradle build files
    ~ Automatically fill "built-against" in plugin annotation
    ~ Correctly generate maven-type sources jar files

---
+++ src/main/java/MyPluginClass.java
@@ -0,0 +1,10 @@
+import de.fearnixx.jeak.reflect.JeakBotPlugin;
+
+/**
+ * Placeholder class.
+ * @deprecated Delete (or replace) this class when implementing your plugin.
+ */
+@JeakBotPlugin(id = "someplugin", builtAgainst = "@JEAK_VERSION@")
+@Deprecated(forRemoval = true)
+public class MyPluginClass {
+}
